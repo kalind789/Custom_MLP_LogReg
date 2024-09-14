0768c4dfe681f21646e5143418b10c783ed92516
kadhika2 <kadhika2@u.rochester.edu>
2024-09-14 14:52:21 -0400

    Created some functions and stuff.

--- src/logreg.py
+++ src/logreg.py
@@ -1,5 +1,19 @@
 import numpy as np
 import pandas as pd
+from util import loadDataset
+
+loans_train = './data/loans.train'
+
+ltrain_data, ltrain_targets = loadDataset(loans_train)
+loans_train_features = pd.DataFrame(data=ltrain_data)
+print(loans_train_features.head())
+print(ltrain_data)
+print(ltrain_targets)
+
+# Logistic Regression
+def LogisticReg(lltrain_targets, loans_train_features):
+    pass
+
 
 # Sigmoid Function
 def sigmoid(n):
@@ -10,7 +24,7 @@ def Error(value):
     np.sum(sigmoid(np.dot(weights, features)))
 
 # Gradient Descent Algorithm
-def gradient_descent():
+def gradient_descent(learning_rate):
     converged = False
     while not converged:
         new_weight = curr_weight + learning_rate * (np.gradient())
